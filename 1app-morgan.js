d8a821bddbb5f255ca2080a868945e71d6018376
dorteks <ayodelealayemi@gmail.com>
2022-03-28 14:30:43 +0100

    middleware

---
+++ 1app-morgan.js
@@ -0,0 +1,50 @@
+ 
+const express = require('express');
+const morgan = require('morgan');
+const res = require('express/lib/response');
+const { home } = require('nodemon/lib/utils');
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views2');
+
+
+app.listen(3000);
+
+
+// app.use(morgan('dev'));
+// app.use(morgan('common'));
+// app.use(morgan('tiny'));
+app.use(morgan('short'));
+
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: "Introduction", snippet: "Welcome to my website, i am writing this code using a tutorial " },
+        {title: "Beginner", snippet: "I started learning node and express few weeks back " },
+        {title: "Assistance/Help", snippet: "Kindly assist/help in any little way you can. Thank you. "},
+    ];
+    res.render('index', { title: "Home", blogs});
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title : "About"});
+})
+
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title : "Create a new blog"});
+})
+
+app.use((req, res) => {
+    res.render('404', { title : "404 page"});
+});
+
+
+
+
+
+
